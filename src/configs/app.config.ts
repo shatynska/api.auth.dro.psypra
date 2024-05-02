665c8ebbf1c210ca293709c5415500ab8ddd11c1
Helen Shatynska <helen.shatynska@gmail.com>
2024-05-02 18:21:43 +0300

    feat: add host & logLevel properties to env & appConfig

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -4,9 +4,13 @@ import { EnvSchema, env } from '../constants/env';
 const appConfigSchema = z.object({
   nodeEnv: EnvSchema.shape.NODE_ENV,
   port: EnvSchema.shape.PORT,
+  host: EnvSchema.shape.HOST,
+  logLevel: EnvSchema.shape.LOG_LEVEL,
 });
 
 export const appConfig: z.infer<typeof appConfigSchema> = {
   nodeEnv: env.NODE_ENV ?? 'development',
   port: env.PORT ?? 3000,
+  host: env.HOST ?? 'localhost',
+  logLevel: env.LOG_LEVEL ?? 'info',
 };
--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,8 +1,10 @@
 import { z } from 'zod';
 
 export const EnvSchema = z.object({
-  NODE_ENV: z.enum(['development', 'test', 'production']),
+  NODE_ENV: z.enum(['development', 'production']),
   PORT: z.coerce.number().positive().max(65536, `port should be >= 0 and < 65536`),
+  HOST: z.string(),
+  LOG_LEVEL: z.enum(['trace', 'debug', 'info', 'warn', 'error', 'fatal', 'silent']),
 });
 
 export const env = EnvSchema.partial().parse(process.env);
